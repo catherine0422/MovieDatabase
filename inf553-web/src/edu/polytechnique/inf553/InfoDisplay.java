c484183c43385e18e32a4776bbfa2cadb5402e3c
yujia.fu <838595821@qq.com>
2020-12-15 21:00:36 +0100

    Clean code for InfoDisplay.java

--- inf553-web/src/edu/polytechnique/inf553/InfoDisplay.java
+++ inf553-web/src/edu/polytechnique/inf553/InfoDisplay.java
@@ -12,53 +12,45 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class InfoDisplay
+ * Returns the personal page for an actor
  */
 @WebServlet("/InfoDisplay")
 public class InfoDisplay extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public InfoDisplay() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * deGet parameter:
+	 *  	@pid: person id 
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		String pid = request.getParameter("pid");
+		
+		// Get personal information with the Postgresql query
 		PersonInfo pif = null;
 		ArrayList<MovieInfo> mif = new ArrayList<>();
 		try {
 			pif = Postgresql.queryPersonInfo(pid);
 			mif = Postgresql.queryMovieInfo(pid);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		// Direct to the InfoDisplay page
 		request.setAttribute("pif", pif);
 		request.setAttribute("mif", mif);
 		request.getRequestDispatcher("InfoDisplay.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
